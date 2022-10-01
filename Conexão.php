3055d628437999c648bcad95273831e623e63518
Galibinja <77208884+Galibinja@users.noreply.github.com>
2022-10-01 19:05:19 -0300

    Update Conexão.php

--- Conexão.php
+++ Conexão.php
@@ -1,3 +1,3 @@
 <?php
- $con = pg_connect("host=ec2-3-93-206-109.compute-1.amazonaws.com port=5432 dbname=d3doj7sr60nnpd user=boupatzxmzekmi password=e0cca602f5fadf5a5a441770cc86406963028ba6286132960371c66e2397f7b7");
+ $con = pg_connect("host=ec2-3-93-206-109.compute-1.amazonaws.com port=5432 dbname=babel password=e0cca602f5fadf5a5a441770cc86406963028ba6286132960371c66e2397f7b7");
 ?>
