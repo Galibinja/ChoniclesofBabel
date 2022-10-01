02a8c75bd7cdf74c2ec755b5ac15fe3e01013bef
Galibinja <77208884+Galibinja@users.noreply.github.com>
2022-10-01 01:53:14 -0300

    Update Conexão.php

--- Conexão.php
+++ Conexão.php
@@ -1,3 +1,13 @@
 <?php
- $con = mysqli_connect('sql313.epizy.com','epiz_32345080','aQGNRK2X8o','epiz_32345080_croniclesofbabel');
+     $servidor = "localhost";
+     $porta = 5432;
+     $bancoDeDados = "d3doj7sr60nnpd";
+     $usuario = "boupatzxmzekmi";
+     $senha = "e0cca602f5fadf5a5a441770cc86406963028ba6286132960371c66e2397f7b7";
+
+     $conexao = pg_connect("host=$servidor port=$porta dbname=$bancoDeDados " +
+                                     "user=$usuario password=$senha");
+     if(!$conexao) {
+         die("Não foi possível se conectar ao banco de dados.");
+     }
 ?>
