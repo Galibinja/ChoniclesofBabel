84f0b63affb4ce43759395819d9981a6f802ca92
Galibinja <77208884+Galibinja@users.noreply.github.com>
2022-10-01 17:28:01 -0300

    Update Conexão.php

--- Conexão.php
+++ Conexão.php
@@ -1,13 +1,3 @@
 <?php
-     $servidor = "ec2-3-93-206-109.compute-1.amazonaws.com";
-     $porta = 5432;
-     $bancoDeDados = "d3doj7sr60nnpd";
-     $usuario = "boupatzxmzekmi";
-     $senha = "e0cca602f5fadf5a5a441770cc86406963028ba6286132960371c66e2397f7b7";
-
-     $conexao = pg_connect("host=$servidor port=$porta dbname=$bancoDeDados " +
-                                     "user=$usuario password=$senha");
-     if(!$conexao) {
-         die("Não foi possível se conectar ao banco de dados.");
-     }
+ $con = pg_connect("host=ec2-3-93-206-109.compute-1.amazonaws.com port=5432 dbname=d3doj7sr60nnpd user=boupatzxmzekmi password=e0cca602f5fadf5a5a441770cc86406963028ba6286132960371c66e2397f7b7");
 ?>
