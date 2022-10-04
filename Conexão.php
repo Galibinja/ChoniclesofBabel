0303ebd1960e11377cf0f12c04215f31b85ac3b2
Galibinja <77208884+Galibinja@users.noreply.github.com>
2022-10-04 07:33:13 -0300

    Update Conexão.php

--- Conexão.php
+++ Conexão.php
@@ -1,3 +1,17 @@
 <?php
- $con = pg_connect("host=ec2-3-93-206-109.compute-1.amazonaws.com port=5432 dbname=babel password=e0cca602f5fadf5a5a441770cc86406963028ba6286132960371c66e2397f7b7");
+ $con = $dbconn = pg_connect("dbname=da8kgo1e67o77e");
+ //connect to a database named "mary"
+ 
+ $dbconn2 = pg_connect("host=ec2-44-208-151-7.compute-1.amazonaws.com port=5432 dbname=da8kgo1e67o77e");
+ // connect to a database named "mary" on "localhost" at port "5432"
+ 
+ $dbconn3 = pg_connect("host=sheep port=5432 dbname=da8kgo1e67o77e user=zjpvfyzaxnxancpassword=foo");
+ //connect to a database named "mary" on the host "sheep" with a username and password
+ 
+ $conn_string = "host=ec2-44-208-151-7.compute-1.amazonaws.com port=5432 dbname=da8kgo1e67o77e user=zjpvfyzaxnxanc password=ca06e6f1b3609cc4dd59103dbbe94534ec193f13e539e9ed268fd4ba0ddf9528";
+ $dbconn4 = pg_connect($conn_string);
+ //connect to a database named "test" on the host "sheep" with a username and password
+ 
+ $dbconn5 = pg_connect("host=ec2-44-208-151-7.compute-1.amazonaws.com options='--client_encoding=UTF8'");
+ //connect to a database on "localhost" and set the command line parameter which tells the encoding is in UTF-8
 ?>
