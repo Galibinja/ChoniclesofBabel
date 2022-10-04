3b13240d645d36320b20c458be5e224381f4cb4e
Galibinja <77208884+Galibinja@users.noreply.github.com>
2022-10-04 07:51:20 -0300

    Update dadoscadastro.php

--- dadoscadastro.php
+++ dadoscadastro.php
@@ -12,12 +12,12 @@ $sql_insert = "INSERT INTO user (usuario,email,senha) VALUES ('$usuario','$email
 
 print_r($sql_insert);
 
-if(mysqli_query($con,$sql_insert)){
+if(pg_query($con,$sql_insert)){
     echo "<script>alert('Cadastrado com sucesso!');window.location='index.php'</script>";
     
 }
 else{
     echo "<script>alert('Erro ao cadastrar, preencha novamente por favor!');window.location='Cadastro.php'</script>";
 }
- mysqli_close($con);
+ pg_close($con);
 ?>
